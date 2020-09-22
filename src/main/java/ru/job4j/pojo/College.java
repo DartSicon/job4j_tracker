3dc37c17ba1fefdfb726b6a3ee7582bd0e66c0a1
DartsiCon <cursedkun@gmail.com>
2020-09-22 23:42:15 +0300

    2. Модель данных. [#351073]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,18 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFio("Иванов Иван Иванович");
+        student.setGroup("Э-102");
+        student.setReceiptDate(new Date());
+
+        System.out.println("Студент: " + student.getFio() + ", " +
+                           "группа: " + student.getGroup() + ", " +
+                           "дата поступления: " + student.getReceiptDate());
+
+    }
+}
