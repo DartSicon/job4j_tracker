b53870e868353a5eb10452712b0528f0fa92e080
DartsiCon <cursedkun@gmail.com>
2020-09-27 22:16:25 +0300

    Исправление кода по ревью [#351079]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -42,6 +42,7 @@ public class Tracker {
         int index = indexOf(id);
         boolean rsl = index != -1;
         if (rsl) {
+            item.setId(id);
             items[index] = item;
         }
         return rsl;
