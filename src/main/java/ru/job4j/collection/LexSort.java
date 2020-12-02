d6bd77df7d91549a6b39fc25bd52548d80805a5d
DartsiCon <cursedkun@gmail.com>
2020-12-02 15:38:32 +0300

    Правка метода сортировки строки по номеру после ревью [#350974]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -6,37 +6,12 @@ public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        StringBuilder leftNumber = new StringBuilder();
-        StringBuilder rightNumber = new StringBuilder();
-        boolean leftNumberFound = false;
-        boolean rightNumberFound = false;
+        String[] leftArray = left.split("\\. ");
+        String[] rightArray = right.split("\\. ");
 
-        for (int i = 0; i < Math.min(left.length(), right.length()); i++) {
-            if (leftNumberFound && rightNumberFound) {
-                break;
-            }
+        int leftNumber = Integer.parseInt(leftArray[0]);
+        int rightNumber = Integer.parseInt(rightArray[0]);
 
-            if (!leftNumberFound) {
-                char leftChar = left.charAt(i);
-
-                if (!Character.isDigit(leftChar)) {
-                    leftNumberFound = true;
-                } else {
-                    leftNumber.append(leftChar);
-                }
-            }
-
-            if (!rightNumberFound) {
-                char rightChar = right.charAt(i);
-
-                if (!Character.isDigit(rightChar)) {
-                    rightNumberFound = true;
-                } else {
-                    rightNumber.append(rightChar);
-                }
-            }
-        }
-
-        return Integer.compare(Integer.parseInt(leftNumber.toString()), Integer.parseInt(rightNumber.toString()));
+        return Integer.compare(leftNumber, rightNumber);
     }
 }
