ddf549f8e016d083a58326f6de2520a650dc1fac
Aleksandr Volodin <cursedkun@gmail.com>
2021-06-26 23:44:59 +0300

    Исправление кода после ревью [#70609 #171179]

--- src/main/java/ru/job4j/lambda/ScopeInside.java
+++ src/main/java/ru/job4j/lambda/ScopeInside.java
@@ -6,13 +6,12 @@ public class ScopeInside {
     public static void main(String[] args) {
         int[] number = {1, 2, 3};
         int total = 0;
-        for (int i = 0; i < number.length; i++) {
-            int num = i;
+
+        for (int num : number) {
             int tempTotal = total;
-            total = add(
-                    () -> tempTotal + num
-            );
+            total = add(() -> tempTotal + num);
         }
+
         System.out.println(total);
     }
 
