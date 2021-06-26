724fe8930cacd331408925cd9a3986d18e4a9955
Aleksandr Volodin <cursedkun@gmail.com>
2021-06-26 23:32:22 +0300

    Исправление кода после ревью [#249208 #171162]

--- src/main/java/ru/job4j/collection/LambdaUsage.java
+++ src/main/java/ru/job4j/collection/LambdaUsage.java
@@ -1,26 +1,21 @@
 package ru.job4j.collection;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class LambdaUsage {
     public static void main(String[] args) {
         Comparator<String> comparator = (left, right) -> {
-            int result = 0;
-
-            for (int i = 0; i < Math.min(left.length(), right.length()); i++) {
-                System.out.println("compare char- " + left.charAt(i) + " : " + right.charAt(i));
-                result = Character.compare(left.charAt(i), right.charAt(i));
-                if (result != 0) {
-                    break;
-                }
-            }
-
-            if (result == 0) {
-                System.out.println("compare size - " + left.length() + " : " + right.length());
-                result = Integer.compare(left.length(), right.length());
-            }
-
-            return result;
+            System.out.println("compare " + left + " : " + right);
+            return left.compareTo(right);
         };
+
+        String[] testStrings = {"Ivanov", "Petrov", "Andreev"};
+
+        Arrays.sort(testStrings, comparator);
+
+        for (String testString : testStrings) {
+            System.out.println(testString);
+        }
     }
 }
